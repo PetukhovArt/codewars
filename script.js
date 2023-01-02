3f7640aa98336c983f5deadde7d48fe8b945886d
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-02 12:35:09 +0500

    tasks: ljs base types

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-const burger = document.querySelector('.burger'),
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close');
-
-burger.addEventListener('click', () => {
-    menu.classList.add('active')
-});
-
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active')
-});
-
-const percents = document.querySelectorAll('.skills__progress-percent'),
-      lines = document.querySelectorAll('.skills__progress-line-in');
-
-percents.forEach( (item, i) => {
-    lines[i].style.width = item.innerHTML;
-});
-
-
+"use strict"  //строгий режим
+let message; //переменная
+message = 'Hello'; // оператор присваивания, присвоили значение переменной
+alert(message); //вызвали переменную по имени
+let greeting = 'Hello'; // объявление перем. и запись данных в нее в одну строку
+let admin;
+let name = 'Djon';
+admin = name;
+alert(admin);
+let ourPlanetName;
+let currentUserName;
