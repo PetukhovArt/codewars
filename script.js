41315c0ab92738eb9373bae500c52c4d9d9c1a3b
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-03 01:31:07 +0500

    tasks: add CW result 8 kyu

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 "use strict"  //строгий режим
 let message; //переменная
 message = 'Hello'; // оператор присваивания, присвоили значение переменной
-alert(message); //вызвали переменную по имени
 let greeting = 'Hello'; // объявление перем. и запись данных в нее в одну строку
 let admin;
 let name = 'Djon';
 admin = name;
-alert(admin);
 let ourPlanetName;
 let currentUserName;
+
+//codeWars results
+//Convert boolean values to strings 'Yes' or 'No'.
+function boolToWord( bool ){
+    return bool ? 'Yes':'No';
+}
