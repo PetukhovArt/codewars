56b36ac01ee5bc1019f159bc88e37bf20cdb79ad
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-03 16:33:33 +0500

    Upd: Area or Perimeter

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 //Convert boolean values to strings 'Yes' or 'No'.
 function boolToWord( bool ){
     return bool ? 'Yes':'No';
-}
+};
 
 //8 kyu MakeUpperCase
 function makeUpperCase(str) {
     return str.toUpperCase();
-  }
+};
+
+//8 kyu Area or Perimeter
+const areaOrPerimeter = function(l , w) {
+    if (l == w) {
+        return (l * w)
+    } else {
+        return ((l + w) * 2)
+    }
+};
