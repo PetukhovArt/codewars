2bee8400409d95de7829754952b1f6d2e361b93d
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-03 16:19:20 +0500

    Upd: add 8 kyu

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-"use strict"  //строгий режим
-let message; //переменная
-message = 'Hello'; // оператор присваивания, присвоили значение переменной
-let greeting = 'Hello'; // объявление перем. и запись данных в нее в одну строку
-let admin;
-let name = 'Djon';
-admin = name;
-let ourPlanetName;
-let currentUserName;
-
 //codeWars results
 //Convert boolean values to strings 'Yes' or 'No'.
 function boolToWord( bool ){
     return bool ? 'Yes':'No';
 }
+
+//8 kyu MakeUpperCase
+function makeUpperCase(str) {
+    return str.toUpperCase();
+  }
