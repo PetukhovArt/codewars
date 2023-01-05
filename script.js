0393f85713bcc3b1ce12809038ee30cf3483a54b
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-05 16:48:05 +0500

    upd : Training JS #7: if..else and ternary operator

--- script.js
+++ script.js
@@ -42,6 +42,14 @@ function trueOrFalse(val){
     return val ? 'true' : 'false';
   }
 
+//8 Training JS #7: if..else and ternary operator
+function saleHotdogs(n){
+    if (n < 5) return n * 100
+    else if (n >= 5 && n < 10) return n * 95
+    else if (n >= 10) return n * 90
+  }
+
 //8
+
 
 
