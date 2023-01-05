17bb3a56be6b556b15f1b583ede34d03ba19d1cd
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-05 20:38:28 +0500

    upd: loop statement --while and do..while

--- script.js
+++ script.js
@@ -61,5 +61,17 @@ function howManydays(month){
      return 31
    }
 
+//8 loop statement --while and do..while
+function padIt(str,n){
+    while (n > 0){
+      if (n % 2){
+        str = '*' + str
+      } else {
+        str = str + '*'
+      }
+      n--;
+    }
+    return str;
+  }
 
 
