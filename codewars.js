c2379a5b2f5ca107ce9178cbc2c90ab54f11d2b9
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-08 17:55:39 +0500

    upd: 8 kyu Training JS #11: loop statement --break,continue

--- codewars.js
+++ codewars.js
@@ -89,3 +89,24 @@ function pickIt(arr){
   }
   return [odd,even]
 }
+
+//8 kyu Training JS #11: loop statement --break,continue
+function grabDoll(dolls){
+  let bag=[];
+  for (let i=0;i<dolls.length;i++) {
+    if (dolls[i]=='Hello Kitty' || dolls[i]=='Barbie doll') {
+      bag.push(dolls[i])
+    } else {
+      continue
+    }
+    if (bag.length==3) {
+      break
+    } else {
+      continue
+    }
+  }
+  return bag;
+}
+
+//8 kyu Training JS #12: loop statement --for..in and for..of
+
