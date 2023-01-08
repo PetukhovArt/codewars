552dd8071f2754466d7e4bcff3105517844c5f9e
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-09 01:17:08 +0500

    upd: Training JS #13: Number object and its properties

--- codewars.js
+++ codewars.js
@@ -117,3 +117,14 @@ function giveMeFive(obj){
     }
   return sentence
 }
+
+//Training JS #13: Number object and its properties
+let whatNumberIsIt = n =>
+`Input number is ${n == Number.POSITIVE_INFINITY ? 'Number.POSITIVE_INFINITY':
+  n == Number.MAX_VALUE ? 'Number.MAX_VALUE':
+  n == Number.MIN_VALUE ? 'Number.MIN_VALUE':
+  n == Number.NEGATIVE_INFINITY ? 'Number.NEGATIVE_INFINITY':
+  Number.isNaN(n) ? 'Number.NaN':
+  n}`
+
+//
