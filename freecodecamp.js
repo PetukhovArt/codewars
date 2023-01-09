6aab53a99ef33b517da6b475a73b4e4ecf1eec02
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-09 14:07:37 +0500

    upd: FCC Adding a Default Option in Switch Statements

--- freecodecamp.js
+++ freecodecamp.js
@@ -40,4 +40,14 @@ function caseInSwitch(val) {
     }
     return answer;
   }
-//
+//Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+    let answer = "";
+    switch (val) {
+    case "a": answer='apple'; break;
+    case "b": answer='bird'; break;
+    case "c": answer='cat'; break;
+    default: answer='stuff'; break;
+    }
+    return answer;
+  }
