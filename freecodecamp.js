f3ceb7383b8b16c62f06d59ada8941853e8eb5ff
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-09 13:14:54 +0500

    upd: FCC Selecting from Many Options with Switch Statements

--- freecodecamp.js
+++ freecodecamp.js
@@ -21,4 +21,22 @@ if (strokes==1) {
   else return "Change Me"
 }
 
-//
+//Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+    let answer = '';
+    switch (val) {
+      case 1:
+      answer='alpha';
+      break;
+      case 2:
+      answer='beta';
+      break;
+      case 3:
+      answer='gamma';
+      break;
+      case 4:
+      answer='delta';
+      break;
+    }
+    return answer;
+  }
