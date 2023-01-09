1bde2048127ad0aab60fd88c551d50d414a68e20
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-09 14:12:43 +0500

    upd: FCC Multiple Identical Options in Switch Statements

--- freecodecamp.js
+++ freecodecamp.js
@@ -51,3 +51,20 @@ function switchOfStuff(val) {
     }
     return answer;
   }
+//Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+    let answer = '';
+    switch(val){
+      case 1:
+      case 2:
+      case 3: answer='Low'; break;
+      case 4:
+      case 5:
+      case 6: answer='Mid'; break;
+      case 7:
+      case 8:
+      case 9: answer='High'; break;
+    }
+    return answer;
+  }
+//Replacing If Else Chains with Switch
