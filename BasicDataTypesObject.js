98f27adeb0c5540c06d2970fd9ef3fc36f5696a3
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-11 13:57:33 +0500

    upd: 8 Training JS #5: Basic data types--Object

---
+++ BasicDataTypesObject.js
@@ -0,0 +1,6 @@
+//8 Training JS #5: Basic data types--Object
+function animal(obj){
+    let animal= {name:"dog",legs: 4, color:"white"};
+    let result ='This ' + obj.color + ' ' + obj.name + ' has ' + obj.legs + ' legs' + '.';
+    return result;
+  }
