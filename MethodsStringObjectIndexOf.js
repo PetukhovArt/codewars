92274c50d14e45cfa405d192d8cec09a8ac68880
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-11 17:58:13 +0500

    upd: 8 kyu Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()

---
+++ MethodsStringObjectIndexOf.js
@@ -0,0 +1,4 @@
+//8 kyu Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()
+function firstToLast(str,c) {
+    return str.includes(c)?str.lastIndexOf(c)-str.indexOf(c): -1;
+  }
