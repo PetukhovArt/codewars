e627ab119702ec8d0396a06b9750a35daafc6161
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-16 14:49:16 +0500

    upd: 7 kyu Training Time

---
+++ TrainingTime.js
@@ -0,0 +1,8 @@
+let shuffleIt=(arr,...arrs) => {
+
+    for (let i=0;i<arrs.length;i++) {
+      let [a,b]=[arrs[i][0],arrs[i][1]];
+      [arr[a],arr[b]]=[arr[b],arr[a]];
+    }
+    return arr;
+  }
