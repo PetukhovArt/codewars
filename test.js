5ef07d5e792ca3b87f6b404046d0200b740c2be2
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-19 16:23:41 +0500

    upd: 8 kyu Total amount of points

--- test.js
+++ test.js
@@ -1,10 +1,3 @@
-let queue=["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"];
-let sheepCount = 0;
-for (let i = queue.length - 1; i >= 0; i--) {
-    if (queue[i]==="sheep") {
-        sheepCount+=1;
-    } else break;
-  };
-console.log(sheepCount);
+
 
 
