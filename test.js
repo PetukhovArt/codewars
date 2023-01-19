3124a19cf853e15e949a81447b80fe32aaf57cca
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-19 15:05:33 +0500

    upd: 8 kyu A wolf in sheep's clothing

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-let arr=['asd', 'abv', 'gande'];
-let word= arr.pop().slice(-1);
-console.log(word);
+let queue=["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"];
+let sheepCount = 0;
+for (let i = queue.length - 1; i >= 0; i--) {
+    if (queue[i]==="sheep") {
+        sheepCount+=1;
+    } else break;
+  };
+console.log(sheepCount);
 
 
