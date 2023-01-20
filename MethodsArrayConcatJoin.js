7a85d72c3851395c001f958d0b4e9a7c6062115a
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-20 18:21:57 +0500

    upd: 8 kyu Training JS #29: methods of arrayObject---concat() and join()

---
+++ MethodsArrayConcatJoin.js
@@ -0,0 +1,6 @@
+function bigToSmall(arr){
+    let newArr=[].concat(...arr).sort( (a, b) => b-a ).join(">")
+     return newArr
+   }
+//https://www.codewars.com/kata/5731861d05d14d6f50000626/train/javascript
+//8 kyu Training JS #29: methods of arrayObject---concat() and join()
