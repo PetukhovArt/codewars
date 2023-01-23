c4aa955beb3c95a6bcbe923d4e14204d1eb85360
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-23 17:36:02 +0500

    upd: add FCC folder and tasks

---
+++ FreeCodeCamp/JsAlgorithmsAndDataStructures/CountingCards.js
@@ -0,0 +1,22 @@
+//Counting Cards
+let count = 0;
+function cc(card) {
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6: count++; break;
+    case 7:
+    case 8:
+    case 9: count+=0; break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A': count--;break;
+  }
+  if (count>0) return `${count} Bet`
+  else if (count<=0) return `${count} Hold`
+}
+
---
+++ FreeCodeCamp/JsAlgorithmsAndDataStructures/ReturnEarlyPatternFunctions.js
@@ -0,0 +1,7 @@
+//Return Early Pattern for Functions
+function abTest(a, b) {
+    if (a<0 || b<0) {
+      return undefined;
+    }
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+    }
