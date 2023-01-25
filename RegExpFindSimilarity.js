7b93518645c36be0137bd2a2de1bc095d8bb3b29
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-25 14:22:43 +0500

    upd: 7 kyu Training JS #38: Regular Expression--"^","$", "." and test()

---
+++ RegExpFindSimilarity.js
@@ -0,0 +1,13 @@
+function findSimilarity(str,word){
+    let regstr=word[0]+Array(word.length-1).join("\\w")+word.slice(-1)
+    //get regExp form from "word"; \\w - check only letters
+    let reg = new RegExp(regstr, "ig")
+    //form the regExp expression
+    let ftr=str.split(" ").filter(el => el.length==word.length).join(" ")
+    //filter str to take words that == word.length
+    return (ftr.match(reg)===null) ? "" : ftr.match(reg).join(" ")
+    //if match  null -> return empty string / else return match!
+  }
+
+//https://www.codewars.com/kata/573975d3ac3eec695b0013e0/train/javascript
+//7 kyu Training JS #38: Regular Expression--"^","$", "." and test()
--- test.js
+++ test.js
@@ -1,14 +1,25 @@
 
-    let str="bag dog dig dot doog dogs"
-    let word="dog"
-    let regstr=word[0]+Array(word.length-1).join(".")+word.slice(-1);
-    let reg1=new RegExp(regstr+"+");
-    let reg2=reg1+"ig"
-    let sim = str.match(reg1);
-    if (reg1.test(str)===false) {
-      console.log("XYI");
-    } else console.log(sim);
+    let str="bag dog dig dot doog dogs";  //bag dog dig dot doog dogs
+    let word="god";
+    let reg2=/g.d\s/ig;   //выдает NULL если нет совпадений
 
-console.log(reg2);
+    console.log (reg2.test(str));
+
+    console.log (str.match(reg2));
+
+
+    // let regstr=word[0]+Array(word.length-1).join(".")+word.slice(-1); // /d.g/gi
+    // let arr=str.split(" ");
+    // let reg1 = new RegExp("^"+regstr+"$", "gi"); //регулярка
+    // let ftr=arr.filter(el => el.length===word.length).join(" ")
+
+// console.log (reg1); //проверяю регулярку
+// console.log (reg1.test(str)); // true/false ДОЛЖНО РАБОТАТЬ СУКА
+// console.log(ftr.match(reg1)); //совпадения
+
+//regexObj.test(str)
+
+
+
 
 
