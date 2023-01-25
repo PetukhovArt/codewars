b6189b96122663ad0093c0693021c87c044b4b76
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-01-25 15:38:57 +0500

    upd: 7 kyu Vowel Count

--- test.js
+++ test.js
@@ -1,23 +1,9 @@
 
-    let str="bag dog dig dot doog dogs";  //bag dog dig dot doog dogs
-    let word="god";
-    let reg2=/g.d\s/ig;   //выдает NULL если нет совпадений
+    let str="abracadabra"
+    let reg=/[aeiou]/g
 
-    console.log (reg2.test(str));
+    console.log (str.match(reg).join("").length);
 
-    console.log (str.match(reg2));
-
-
-    // let regstr=word[0]+Array(word.length-1).join(".")+word.slice(-1); // /d.g/gi
-    // let arr=str.split(" ");
-    // let reg1 = new RegExp("^"+regstr+"$", "gi"); //регулярка
-    // let ftr=arr.filter(el => el.length===word.length).join(" ")
-
-// console.log (reg1); //проверяю регулярку
-// console.log (reg1.test(str)); // true/false ДОЛЖНО РАБОТАТЬ СУКА
-// console.log(ftr.match(reg1)); //совпадения
-
-//regexObj.test(str)
 
 
 
---
+++ vowelCount.js
@@ -0,0 +1,3 @@
+//7 kyu Vowel Count
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript/
+let getCount=(str)=>str.match(/[aeiou]/g).join("").length;
