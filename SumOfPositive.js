f0f74625a2c81ebb04aaa2349f3dd38d9fee0922
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-02-06 08:42:54 +0500

    add: 8 kyu Sum of positive

---
+++ SumOfPositive.js
@@ -0,0 +1,7 @@
+function positiveSum(arr) {
+    return arr.filter(el => el>0).reduce((a,b)=>a+b,0)
+  }
+
+//   https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+//   8 kyu
+//   Sum of positive
