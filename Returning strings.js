c67ce03fda22c8c85be5e52fb38e877af0ac377b
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-02-25 11:42:54 +0500

    feat: add 3 solutions

---
+++ Returning strings.js
@@ -0,0 +1,7 @@
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
+
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+//     8 kyu
+// Returning Strings
