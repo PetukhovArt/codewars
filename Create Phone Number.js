b698d8250150ec6780763cb719e98d5e7b7dc978
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-03-11 23:25:01 +0500

    feat: add solutions

---
+++ Create Phone Number.js
@@ -0,0 +1,5 @@
+// 6 kyu
+// Create Phone Number
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83/solutions/javascript?filter=me&sort=newest&invalids=false
+
+const createPhoneNumber = (n) => `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`
