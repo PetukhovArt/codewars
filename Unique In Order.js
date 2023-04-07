451d96d5532ec13e4efe94ce06e0bde56101df7f
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-04-07 14:55:28 +0500

    feat: add 7, 6 kyu

---
+++ Unique In Order.js
@@ -0,0 +1,15 @@
+
+// https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
+//     6 kyu
+// Unique In Order
+
+
+const uniqueInOrder=function(iterable){
+    return (typeof iterable == "object") ?
+        iterable.filter((v,i,a) => a[i] !== a[i+1])
+        : iterable.split("").filter((v,i,a) => {
+            console.log(a[i], a[i+1], a[i] !==a[i+1] ? `'Letter : '${a[i]} ' - TRUTH'`: '')
+            //log return the understanding of this filter iterations
+            return a[i] !== a[i+1]
+        })
+}
